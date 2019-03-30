54f2da1af50615e7226d3545ad36a120e5748568
Abhishek Raj <abhi@raj.me>
2019-03-30 17:14:03 +0530

    Setup React

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-console.log('Hello Node.js project.');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Hello from React!';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+console.log(title);
 console.log(process.env.MY_SECRET);
